519ed4a1621ad1d246dbe0497934683259667ae2
gorbiromaricauf <gorbiromaric@gmail.com>
2014-07-16 14:32:03 +0700

    modified:   css/mobilestyle.css 	modified:   index.html 	modified:   jm/function.js

--- jm/function.js
+++ jm/function.js
@@ -5,7 +5,7 @@
 $(function() {
 	//$.mobile.page.prototype.options.domCache = true;
 	//active = $.mobile.activePage.attr('id')+'.html';
-	racine ="https://outils.vn.auf.org/media/";
+	racine ="http://outils.vn.auf.org/media/";
 	
 	var viewport = {
     width  : $(window).width(),
@@ -13,9 +13,9 @@ $(function() {
 	};
 	
 
-	 url = "http://outils.vn.auf.org/news/api/?format=jsonp";
-	 ajax();
 	
+	 ajaxNews();
+	ajaxSA();
    $(document).on('click','#news ul li a', function () {
            
 		    siteweb = $(this).attr('data-title');
@@ -56,7 +56,48 @@ $(function() {
 			
 	
 });
-function ajax(){
+function ajaxSA(){
+	var url = 'https://outils.vn.auf.org/veille/api_sa/?format=jsonp';
+	 $.ajax({
+		type: 'GET',
+		dataType: "jsonp",
+		url: url,
+		crossDomain: true,
+		jsonp: 'callback', 
+		cache: false,
+		success: function (responseData, textStatus, jqXHR) {			
+				
+		
+			var data = responseData.results;
+			var resultat = '<div data-role="tabs" id="tabs"><div data-role="navbar"  style="position:fixed;width:95%;z-index:10000;height:80px;"><ul>';
+			for(i=0;i<data.length;i++ ){
+				resultat +='<li><a href="#pays'+data[i].id+'" >'+data[i].pays+'</a></li>';
+			
+				
+				}
+					
+			resultat+='</ul>  </div>';
+  			for(i=0;i<data.length;i++ ){
+				resultat+='<div id="pays'+data[i].id+'" class="ui-body-d ui-content " style="top:100px;height:device-height;overflow:auto;-webkit-overflow-scrolling: touch;">';
+					resultat+='<h2>'+data[i].pays+'</h2>';
+					resultat+= data[i].contenu_mobile;
+ 				 resultat+='</div>';
+			
+				
+			}
+				 resultat+='</div>';
+    		$('#systemeinfos').html(resultat);
+
+		},
+		error: function (responseData, textStatus, errorThrown) {
+				
+			 alert('POST failed.'+errorThrown);
+		}
+		});
+}
+function ajaxNews(){
+	 var  url = "https://outils.vn.auf.org/news/api/?format=jsonp";
+	 
 	 $.ajax({
 				type: 'GET',
 				dataType: "jsonp",
@@ -66,17 +107,17 @@ function ajax(){
 				cache: false,
 				success: function (responseData, textStatus, jqXHR) {			
 						
-						var resultat ='<ul data-role="listview" data-inset="true">';
+						var resultat ='<ul data-role="listview" data-inset="true" class="listnews">';
 						var data = responseData.results;
 						for(i=0;i<data.length;i++ ){
 					 		
-							resultat+= '<li><a href="#"  data-title="'+data[i].lien_vers_site+'" data-ajax="false"><img src="'+racine+data[i].images+'"> <h2>'+data[i].titre+'</h2><p>'+data[i].extrait_contenu+'</p></a> </li>';
+							resultat+= '<li><a href="#"  data-title="'+data[i].lien_vers_site+'" data-ajax="false"><img src="'+racine+data[i].images+'"> <h2>'+data[i].titre+'</h2><p >'+data[i].extrait_contenu.substring(80,-1)+'</p></a> </li>';
 						
 						
 						}
 							resultat+='</ul>';
 						// $("[data-role='listview']").html(resultat);
-						 $("#news").html(resultat).find("ul").listview();
+						 $("#news").html(resultat).find("ul").addClass('listnews');
 						
 				},
 				error: function (responseData, textStatus, errorThrown) {
